bfe4b862c553eab31f9b5ac6e2f622e1ab856b90
Sol <faisol.chehumar@gmail.com>
2019-05-23 00:38:53 +0700

    Add Error handle

--- services/movieService.js
+++ services/movieService.js
@@ -1,25 +1,33 @@
 const axios = require('axios');
 
 async function getMovies (limit = 5) {
-  const result = await axios.get(`https://api.avgle.com/v1/videos/0?limit=${limit}`);
-  const viedos = result.data.response.videos;
-  const movies = viedos.map(video => ({
-    img: video.preview_url,
-    link: video.video_url,
-  }));
+  try {
+    const result = await axios.get(`https://api.avgle.com/v1/videos/0?limit=${limit}`);
+    const viedos = result.data.response.videos;
+    const movies = viedos.map(video => ({
+      img: video.preview_url,
+      link: video.video_url,
+    }));
 
-  return movies;
+    return movies;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getMoviesByKeyword (query, limit = 5) {
-  const result = await axios.get(`https://api.avgle.com/v1/search/${encodeURIComponent(query)}/1?limit=${limit}`);
-  const viedos = result.data.response.videos;
-  const movies = viedos.map(video => ({
-    img: video.preview_url,
-    link: video.video_url,
-  }));
+  try {
+    const result = await axios.get(`https://api.avgle.com/v1/search/${encodeURIComponent(query)}/1?limit=${limit}`);
+    const viedos = result.data.response.videos;
+    const movies = viedos.map(video => ({
+      img: video.preview_url,
+      link: video.video_url,
+    }));
 
-  return movies;
+    return movies;
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
